94fa44d3f5180f889cf11c6de6ea0bcd475fdd27
BANDARAKDT <104091741+BANDARAKDT@users.noreply.github.com>
2024-01-24 13:42:14 +0530

    commit of methods on  24/01/2024

---
+++ Methods.java
@@ -0,0 +1,12 @@
+public class Methods {
+
+
+
+    static void Meth1(){
+
+        System.out.println("this is the first method which is calling from main method");
+    }
+    public static void main(String[] args) {
+        Meth1();
+    }
+}
