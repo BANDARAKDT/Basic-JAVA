64e75ed8d887eba28a6b70a464373d0b5c12c573
BANDARAKDT <deshanibandarawas@gmail.com>
2024-01-28 12:14:16 +0530

    static folder

---
+++ SaticVari.java
@@ -0,0 +1,20 @@
+public class SaticVari {
+    int x;
+    static int y;
+    void meth1(){
+
+    }
+
+    static void meth2(){
+            System.out.println("this is a static method");
+
+    }
+    public static void main(String args[]){
+            y=5;
+            meth2();
+
+
+
+
+    }
+}
--- UserInput.java
+++ UserInput.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 
 
 public class UserInput {
-    public static void main(){
-        System.out.Println("this is the code to gather informations from user");
-    Scanner obj= new obj();
-    String input1=obj.nextLine();
-    System.out.Println(input1);
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        String name= obj.nextLine();
+        System.out.println("you entered "+name);
     }
 }
