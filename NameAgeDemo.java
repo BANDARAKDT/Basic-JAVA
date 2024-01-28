b9adefa770d4a68c5568ee650874952f7e3421c6
BANDARAKDT <deshanibandarawas@gmail.com>
2024-01-28 16:20:26 +0530

    public and the private variables

---
+++ NameAgeDemo.java
@@ -0,0 +1,11 @@
+public class NameAgeDemo {
+    
+
+public static void main(String args[]){
+
+    NameAgeDemo obj1=new NameAgeDemo();
+    obj.setName("dela");
+    System.out.println(obj.getName);
+}
+
+}
