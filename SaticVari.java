40be40cfc8080e65a536ea6a493a4c9e1d7dcf49
BANDARAKDT <deshanibandarawas@gmail.com>
2024-01-28 12:32:41 +0530

    static and non static methods

--- SaticVari.java
+++ SaticVari.java
@@ -1,16 +1,22 @@
 public class SaticVari {
     int x;
     static int y;
-    void meth1(){
-
+    static void meth1(){
+        System.out.println("this is a non-static method");
     }
 
     static void meth2(){
             System.out.println("this is a static method");
 
+             
     }
     public static void main(String args[]){
-            y=5;
+           System.out.println("this is main method");
+           System.out.println("Y variable value is "+y);
+        
+           SaticVari obj1=new SaticVari();
+           SaticVari.meth1();
+        y=5;
             meth2();
 
 
