b6779dded53e4ffd1ab4a1bc34ed0ed397150c3d
BANDARAKDT <deshanibandarawas@gmail.com>
2024-01-28 09:16:31 +0530

    user input

--- UserInput.java
+++ UserInput.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
+
 public class UserInput {
     public static void main(){
         System.out.Println("this is the code to gather informations from user");
-    
+    Scanner obj= new obj();
+    String input1=obj.nextLine();
+    System.out.Println(input1);
     }
 }
