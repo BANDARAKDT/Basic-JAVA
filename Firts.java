439bb56b0eae43cf3f84ce91452b4ddf7a588be9
BANDARAKDT <deshanibandarawas@gmail.com>
2024-01-28 10:24:02 +0530

    create Switch

--- Firts.java
+++ Firts.java
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
- public class First{
-    public static void main(String args[]){
 
-        System.out.println("first java code");
-    }
-
-
-
-=======
  class First{
     public static void main(String args[]){
 
@@ -16,5 +7,4 @@
 
 
 
->>>>>>> 32cfeed22d157448a42dc96fdc85a301ee6396e8
 }
