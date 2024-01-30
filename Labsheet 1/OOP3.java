0b7d2025517d5c84c822cc47a47d86d0e41385ee
BANDARAKDT <deshanibandarawas@gmail.com>
2024-01-30 20:56:20 +0530

    Labsheet1:OOP:Q3

---
+++ Labsheet 1/OOP3.java
@@ -0,0 +1,19 @@
+public class OOP3 {
+    String name="Deshani";
+    int age=24;
+
+    public void PersonDetails(String name,int age){
+        this.name=name;
+        this.age=age;
+    } 
+
+    public void displayDetails(){
+        System.out.println("person name:"+name+" and the age is:"+age);
+    }
+public static void main(String[] args) {
+   OOP3 display=new OOP3();
+   display.displayDetails();
+}
+   
+    
+}
