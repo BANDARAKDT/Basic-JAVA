f4475f1dd5f1cbf4f47b66d6c168c11102e612d1
BANDARAKDT <deshanibandarawas@gmail.com>
2024-02-01 23:35:40 +0530

    Cellular plan

--- Labsheet 3/Cellularplan.java
+++ Labsheet 3/Cellularplan.java
@@ -1,5 +1,10 @@
 public class Cellularplan {
 
-    
+    proteced double rate;
+    abstract void getRate();
+
+    public void processBill(int minutes){
+        System.out.println(minutes*rate);
+    }
     
 }
