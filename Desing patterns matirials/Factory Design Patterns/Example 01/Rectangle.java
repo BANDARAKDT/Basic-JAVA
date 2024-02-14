297cd812190f479a99d3f5e2c0b183c6876279b5
BANDARAKDT <deshanibandarawas@gmail.com>
2024-02-14 23:00:39 +0530

    factory design

--- Desing patterns matirials/Factory Design Patterns/Example 01/Rectangle.java
+++ Desing patterns matirials/Factory Design Patterns/Example 01/Rectangle.java
@@ -2,7 +2,8 @@ public class Rectangle implements Shape{
 
     @Override
     public void draw() {
-       System.out.println("Inside Rectangle: draw() method");}
+       System.out.println("Inside Rectangle: draw() method");
+    }
 
     
 }
