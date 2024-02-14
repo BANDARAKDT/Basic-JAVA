10b27d0d938b1db4676373f6d23483879bec45ce
BANDARAKDT <deshanibandarawas@gmail.com>
2024-02-14 19:48:00 +0530

    singleton 2024.02.14

--- Desing patterns matirials/Singleton Design Patterns/Example 01/Singleton.java
+++ Desing patterns matirials/Singleton Design Patterns/Example 01/Singleton.java
@@ -1,14 +1,17 @@
 public class Singleton{
     public static void main(String[] args) {
         A a1=A.getInstance();
-   
+   System.out.println(a1.age);
         A a2=A.getInstance();
+        System.out.println(a2.age);
        
       
     }
 }
 
 class A{
+    int age=10;
+
 private static A a=new A();
 
 private A(){
@@ -18,4 +21,9 @@ private A(){
 public static A getInstance(){
     return a; 
 }
+
+public int getAge(){
+    return age;
 }
+}
+
