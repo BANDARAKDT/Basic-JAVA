59f2c4c38ba850f58a635b3dd2167f20fcba44ba
BANDARAKDT <deshanibandarawas@gmail.com>
2024-02-16 21:53:08 +0530

    2024.02.16 adapter desing pattern

---
+++ Desing patterns matirials/Adapter Design Patterns/CelciusReporter.java
@@ -0,0 +1,14 @@
+public class CelciusReporter{
+    double temperatureInC;
+    public CelciusReporter(){
+
+    }
+
+    public double getTemperature(){
+        return temperatureInC;
+    }
+
+    public void setTemperature(double temperatureInC){
+        this.temperatureInC=temperatureInC;
+    }
+}
