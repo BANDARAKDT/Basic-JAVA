fbad8be7f9c30843ef4a63083094e4df6a5afe48
BANDARAKDT <deshanibandarawas@gmail.com>
2024-02-19 00:55:01 +0530

    thread

--- Threads/Example01/Example01/CountDownTimer.java
+++ Threads/Example01/Example01/CountDownTimer.java
@@ -10,6 +10,16 @@ public class CountDownTimer extends Thread{
     public void run(){
             for(int t=20;t>=0;t--){
                 System.out.println("T minus "+t);
+                if(t==16){
+                    System.out.println("Flood the launch pad");
+
+                }else if(t==6){
+                    System.out.println("start the engines");
+
+                }else if(t==0){
+                    System.out.println("launch");
+                }
+               
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
