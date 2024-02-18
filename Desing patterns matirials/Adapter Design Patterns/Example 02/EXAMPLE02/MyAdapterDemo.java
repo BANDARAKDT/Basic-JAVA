156e283adf6e7f7739d4a3d65a88135630c02408
BANDARAKDT <deshanibandarawas@gmail.com>
2024-02-18 21:56:17 +0530

    Adapter DP

---
+++ Desing patterns matirials/Adapter Design Patterns/Example 02/EXAMPLE02/MyAdapterDemo.java
@@ -0,0 +1,9 @@
+package EXAMPLE02;
+
+public class MyAdapterDemo{
+public static void main(String[] args) {
+    SimpleName simple=new SimpleName();
+    simple.setName("dela");
+    System.out.println("My simplename is "+ simple.getName());
+}
+}
