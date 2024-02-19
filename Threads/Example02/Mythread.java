06cf897021b7bde82425552fec2e6b7bae78562e
BANDARAKDT <deshanibandarawas@gmail.com>
2024-02-19 08:55:21 +0530

    thread priority

--- Threads/Example02/Mythread.java
+++ Threads/Example02/Mythread.java
@@ -3,7 +3,7 @@ public class Mythread extends Thread{
     @Override
     public void run() {
         for (int i = 0; i <10; i++) {
-            System.out.println(Thread.currentThread().getName());
+            System.out.println(Thread.currentThread().getName()+":"+i);
         }
     }
 
