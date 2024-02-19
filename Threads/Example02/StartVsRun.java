d0f807eb79a97c14f8519afaea1c90dd7711b29d
BANDARAKDT <deshanibandarawas@gmail.com>
2024-02-19 08:59:30 +0530

    priority thread

--- Threads/Example02/StartVsRun.java
+++ Threads/Example02/StartVsRun.java
@@ -4,7 +4,14 @@ public class StartVsRun {
         Thread thread1=new Mythread();
         Thread thread2=new Mythread();
 
+thread1.setName("T1");
+thread2.setName("T2");
+
+thread1.setPriority(4);
+thread2.setPriority(Thread.MAX_PRIORITY);
+
+       
         thread1.start();
-        thread2.start();
+        thread2.start();    //cant call start twise it will give u asn error
     }
 }
