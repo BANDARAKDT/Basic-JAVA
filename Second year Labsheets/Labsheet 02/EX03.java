ea8779c0ec77e478b77d940bfeb3b5536d61851f
BANDARAKDT <deshanibandarawas@gmail.com>
2024-02-25 23:13:19 +0530

    labsheet 02

---
+++ Second year Labsheets/Labsheet 02/EX03.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class EX03 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("please enter any number that you need");
+        System.out.println("01.Area of Squre\n02.Area of Circle\n03.Area of Rectangle\n04.Area of Cylinder\n05.Exit");
+   
+   int choice=scanner.nextInt();
+   switch(choice){
+    
+   }
+    }
+}
