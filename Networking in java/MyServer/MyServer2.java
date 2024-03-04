7dae985422227b71680d2929516786d4a3608315
BANDARAKDT <deshanibandarawas@gmail.com>
2024-03-04 09:44:26 +0530

    MyServer2

---
+++ Networking in java/MyServer/MyServer2.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MyServer2{
+
+    public static void main(String args[]){
+        try{
+
+            ServerSocket ss=new ServerSocket(9090);
+
+            Socket s=ss.accept();
+
+            DataInputStream din=new DataInputStream(s.getInputStream());
+            DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+            String str1=" ",str2="";
+            while(!str1.equals("404")){
+                str1=din.readUTF();
+                System.out.println("Client says"+str1);
+                str2=br.readLine();
+                dout.writeUTF(str2);
+                dout.flush();
+
+            }
+            din.close();
+            s.close();
+            ss.close();
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+
+}}
